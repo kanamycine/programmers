9c20a81638039cd40db0bbb3ec6bf9e54d7d9d54
kanamycine <jinyeong5320@gmail.com>
2021-05-20 23:26:44 +0900

    tomorrow is friday

---
+++ 제일작은수제거.py
@@ -0,0 +1,14 @@
+def solution(arr):
+    lst = arr
+    min_value = min(lst)
+
+    if len(lst) == 1:
+        answer = [-1]
+    else:
+        for i in range(len(lst)):
+            if lst[i] == min_value:
+                del lst[i]
+                break
+        answer = lst
+
+    return answer
