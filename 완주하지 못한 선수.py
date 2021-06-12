de83104968f21f008b31f32f58e858768a1ec5bb
kanamycine <jinyeong5320@gmail.com>
2021-06-12 18:20:17 +0900

    완주하지 못한 선수

--- 완주하지 못한 선수.py
+++ 완주하지 못한 선수.py
@@ -1,5 +1,23 @@
-def solution(participant, completion):
-    answer = ''
+# def solution(participant, completion):
+#     answer = ''
 
-    return answer
+#     return answer
+import collections
+answer = ''
+participant = ["marina", "josipa", "nikola", "vinko", "filipa"]
+completion = ["josipa", "filipa", "marina", "nikola"]
 
+participant.sort()
+completion.sort()
+
+a = collections.Counter()
+a.update(participant)
+b = collections.Counter()
+b.update(completion)
+result = a-b
+
+answer = list(result)[0]
+
+print(a)
+print(b)
+print(answer)
